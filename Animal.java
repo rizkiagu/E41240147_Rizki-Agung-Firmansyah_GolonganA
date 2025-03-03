380e095227b9d8f7e40b807f96d5121cfae82553
rizkiagu <rizkiaf73@gmail.com>
2025-03-03 21:51:50 +0700

    Tugas 3

---
+++ Animal.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semester2;
+
+public class Animal {
+    public void makeNoise() {
+        System.out.println("talk");
+    }
+}
+
