8ec5a7b834158d170cb844ad2cc3f30790ce71c9
rizkiagu <rizkiaf73@gmail.com>
2025-03-03 21:53:07 +0700

    Tugas 3

---
+++ Dog.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semester2;
+
+public class Dog extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
